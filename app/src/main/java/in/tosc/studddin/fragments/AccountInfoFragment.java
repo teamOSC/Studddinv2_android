7901375c56ade6073577115f4441262269814903
Tanuj <tj3895@gmail.com>
2015-01-25 19:28:02 +0530

    added skeletal edit buttons
    Signed-off-by: Tanuj <tj3895@gmail.com>

--- app/src/main/java/in/tosc/studddin/fragments/AccountInfoFragment.java
+++ app/src/main/java/in/tosc/studddin/fragments/AccountInfoFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.parse.ParseUser;
 
@@ -20,7 +21,8 @@ import in.tosc.studddin.R;
 public class AccountInfoFragment extends Fragment {
 
     private Button editInfo;
-    private EditText eName,ePassword,eEmail,eInterests;
+    private EditText eName,ePassword,eEmail,eInterests,eUid;
+    private Button editName,editUid,editEmail,editInterest,editPassword;
     private View rootView;
 
     public AccountInfoFragment() {
@@ -28,6 +30,7 @@ public class AccountInfoFragment extends Fragment {
     }
 
     private static final String USER_NAME = "NAME";
+    private static final String USER_UID = "USER NAME";
     private static final String USER_PASSWORD = "NAME";
     private static final String USER_INSTITUTE = "INSTITUTE";
     private static final String USER_EMAIL = "EMAIL";
@@ -42,8 +45,9 @@ public class AccountInfoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         rootView = inflater.inflate(R.layout.fragment_account_info, container, false);
-
+        userInfo = new HashMap<>();
         init();
+        fetchInfoFromParse();
 
         return rootView;
     }
@@ -59,6 +63,8 @@ public class AccountInfoFragment extends Fragment {
             userInfo.put(USER_NAME,currentUser.getString(USER_NAME));
             eName.setText(userInfo.get(USER_NAME));
             userInfo.put(USER_INSTITUTE,currentUser.getString(USER_INSTITUTE));
+            userInfo.put(USER_UID,currentUser.getUsername());
+
 
 
             userInfo.put(USER_QUALIFICATIONS,currentUser.getString(USER_QUALIFICATIONS));
@@ -72,18 +78,30 @@ public class AccountInfoFragment extends Fragment {
         }
     }
 
-    private void init()
-    {
-        editInfo = (Button)rootView.findViewById(R.id.button_edit_info);
-        eName = (EditText)rootView.findViewById(R.id.account_info_name);
-        ePassword = (EditText)rootView.findViewById(R.id.account_info_password);
-        eEmail = (EditText)rootView.findViewById(R.id.account_info_email);
-        eInterests = (EditText)rootView.findViewById(R.id.account_info_interests);
+    private void init() {
+        editInfo = (Button) rootView.findViewById(R.id.button_edit_info);
+        eName = (EditText) rootView.findViewById(R.id.account_info_name);
+        editName = (Button) rootView.findViewById(R.id.edit_name_button);
+
+        ePassword = (EditText) rootView.findViewById(R.id.account_info_password);
+        editPassword = (Button) rootView.findViewById(R.id.edit_user_name);
+
+        eEmail = (EditText) rootView.findViewById(R.id.account_info_email);
+        editEmail = (Button) rootView.findViewById(R.id.edit_email_button);
+
+
+        eInterests = (EditText) rootView.findViewById(R.id.account_info_interests);
+        editInterest = (Button) rootView.findViewById(R.id.edit_interest_button);
+
+        eUid = (EditText) rootView.findViewById(R.id.account_info_user_name);
 
         eName.setEnabled(false);
         ePassword.setEnabled(false);
         eInterests.setEnabled(false);
         eEmail.setEnabled(false);
+        eUid.setEnabled(false);
+
+
 
         editInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,11 +111,21 @@ public class AccountInfoFragment extends Fragment {
                 ePassword.setEnabled(true);
                 eInterests.setEnabled(true);
                 eEmail.setEnabled(true);
+                eUid.setEnabled(true);
             }
         });
     }
 
+    private void changeAttribute(EditText e,String attr)
+    {
+        if(attr.equals(USER_EMAIL) || attr.equals(USER_NAME))
+        {
+            if(e.getText().toString().isEmpty())
+            Toast.makeText(getActivity(),attr + "cannot be empty",Toast.LENGTH_LONG);
+        }
 
-
-
+        ParseUser cu = ParseUser.getCurrentUser();
+        cu.put(attr,e.getText().toString());
+        cu.saveEventually();
+    }
 }
