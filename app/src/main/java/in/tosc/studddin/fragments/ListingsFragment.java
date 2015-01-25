f72177da0c34bfaecba79666c14061afb2f35a3e
Prempal Singh <prempal.42@gmail.com>
2015-01-25 10:22:11 +0530

    Added Upload Fragment+ActionBar buttons

--- app/src/main/java/in/tosc/studddin/fragments/ListingsFragment.java
+++ app/src/main/java/in/tosc/studddin/fragments/ListingsFragment.java
@@ -3,26 +3,24 @@ package in.tosc.studddin.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import in.tosc.studddin.R;
+import in.tosc.studddin.fragments.listings.ListingsSearchFragment;
+import in.tosc.studddin.fragments.listings.ListingsUploadFragment;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListingsFragment extends Fragment {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    View rootView;
 
+    ViewPager notesPager;
+    FragmentPagerAdapter fragmentPagerAdapter;
 
     public ListingsFragment() {
         // Required empty public constructor
@@ -32,73 +30,38 @@ public class ListingsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_notes, container, false);
 
-        rootView = inflater.inflate(R.layout.fragment_listings, container, false);
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.listing_recycler_view);
-
-        mLayoutManager = new LinearLayoutManager(getActivity());
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        ListingInfo[] listing = new ListingInfo[10];
-        for (int i = 0; i < 10; ++i) {
-            listing[i] = new ListingInfo();
-        }
-
-        mAdapter = new ListingAdapter(listing);
-        mRecyclerView.setAdapter(mAdapter);
-        return rootView;
-    }
-
-
-    public class ListingInfo {
-
-        protected String owner_name;
-        protected String listing_name;
-        protected String mobile;
-        protected String distance;
-        protected ImageView listing_image;
-
-
-    }
-
-    public class ListingAdapter extends RecyclerView.Adapter<ListingAdapter.ViewHolder>{
-
-
-        private ListingInfo[] mDataset;
-
-        public ListingAdapter(ListingInfo[] dataSet) {
-            mDataset = dataSet;
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            CardView v = (CardView) LayoutInflater.from(viewGroup.getContext())
-                    .inflate(R.layout.listing_card_view, viewGroup, false);
-
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, int i) {
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDataset.length;
-        }
-
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-
-            public CardView mCardView;
-            public ViewHolder(CardView v) {
-                super(v);
-                mCardView = v;
+        fragmentPagerAdapter = new FragmentPagerAdapter(getChildFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0: return (new ListingsSearchFragment());
+                    case 1: return (new ListingsUploadFragment());
+                }
+                return new ListingsSearchFragment();
             }
-        }
+
+            @Override
+            public int getCount() {
+                return 2;
+            }
+        };
+
+        notesPager = (ViewPager) rootView.findViewById(R.id.notes_pager);
+        notesPager.setAdapter(fragmentPagerAdapter);
+
+
+
+        return rootView;
+
+
     }
+
+
+
+
 
 
 }
---
+++ app/src/main/java/in/tosc/studddin/fragments/listings/ListingsUploadFragment.java
@@ -0,0 +1,108 @@
+package in.tosc.studddin.fragments.listings;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import in.tosc.studddin.R;
+
+/**
+ * Created by Prempal on 1/25/2015.
+ */
+public class ListingsUploadFragment extends Fragment implements View.OnClickListener {
+
+    private ImageView camera;
+    private ImageView upload;
+    private ImageView sdCard;
+    private EditText listing;
+    private EditText mobile;
+    private EditText name;
+
+    public static ImageView listing_image;
+
+    public static String mCurrentPhotoPath;
+
+
+    public ListingsUploadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView =  inflater.inflate(R.layout.fragment_listings_upload, container, false);
+        camera = (ImageView) rootView.findViewById(R.id.listing_camera);
+        upload = (ImageView) rootView.findViewById(R.id.listing_upload);
+        sdCard = (ImageView) rootView.findViewById(R.id.listing_sdcard);
+        listing = (EditText) rootView.findViewById(R.id.et_listing);
+        mobile = (EditText) rootView.findViewById(R.id.et_mobile);
+        name = (EditText) rootView.findViewById(R.id.et_name);
+        listing_image = (ImageView) rootView.findViewById(R.id.listing_image);
+
+        camera.setOnClickListener(this);
+        upload.setOnClickListener(this);
+        sdCard.setOnClickListener(this);
+
+        return rootView;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.listing_camera :
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                            Uri.fromFile(photoFile));
+                    getActivity().startActivityForResult(takePictureIntent, 0);
+                }
+                break;
+
+            case R.id.listing_sdcard :
+                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                getActivity().startActivityForResult(i,1);
+                break;
+        }
+
+    }
+
+        private File createImageFile() throws IOException {
+            // Create an image file name
+            String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            String imageFileName = "JPEG_" + timeStamp + "_";
+            File storageDir = Environment.getExternalStoragePublicDirectory(
+                    Environment.DIRECTORY_PICTURES);
+            File image = File.createTempFile(
+                    imageFileName,  /* prefix */
+                    ".jpg",         /* suffix */
+                    storageDir      /* directory */
+            );
+
+            // Save a file: path for use with ACTION_VIEW intents
+            mCurrentPhotoPath = image.getAbsolutePath();
+            return image;
+        }
+}
