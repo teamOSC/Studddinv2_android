fcd1438d8fbfef9370df4e0434f59b0303aad22f
yogeshbalan <yogeshbalan@live.co.uk>
2015-01-26 02:21:35 +0530

    newTheme and more fixes

--- app/src/main/java/in/tosc/studddin/fragments/PeopleFragment.java
+++ app/src/main/java/in/tosc/studddin/fragments/PeopleFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -37,11 +36,12 @@ public class PeopleFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_people, container, false);
+        /*
         toolbar = (Toolbar) view.findViewById(R.id.toolbar);
         ((ActionBarActivity) getActivity()).setSupportActionBar(toolbar);
         toolbar.setTitleTextColor(getResources().getColor(R.color.white));
         toolbar.setNavigationIcon(R.drawable.ic_drawer);
-
+        */
 
         fragmentPagerAdapter = new FragmentPagerAdapter(getChildFragmentManager()) {
             @Override
