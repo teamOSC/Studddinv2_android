d57591e342242b09a1c6b7e8eb9f8c15b4fb62b2
sidharth <sidharth15.raja@gmail.com>
2015-01-25 17:04:04 +0530

    added skeletal layout

--- app/src/main/java/in/tosc/studddin/fragments/AccountInfoFragment.java
+++ app/src/main/java/in/tosc/studddin/fragments/AccountInfoFragment.java
@@ -6,11 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import in.tosc.studddin.R;
 
 
 public class AccountInfoFragment extends Fragment {
+
+    private Button editInfo;
+    private TextView eName,ePassword,eEmail,eInterests;
+    private View rootView;
 
     public AccountInfoFragment() {
         // Required empty public constructor
@@ -21,8 +30,31 @@ public class AccountInfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account_info, container, false);
+        rootView = inflater.inflate(R.layout.fragment_account_info, container, false);
+
+        init();
+
+        return rootView;
     }
+
+    private void init()
+    {
+        editInfo = (Button)rootView.findViewById(R.id.button_edit_info);
+        eName = (EditText)rootView.findViewById(R.id.account_info_name);
+        ePassword = (EditText)rootView.findViewById(R.id.account_info_password);
+        eEmail = (EditText)rootView.findViewById(R.id.account_info_email);
+        eInterests = (EditText)rootView.findViewById(R.id.account_info_interests);
+
+
+        editInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+
 
 
 }
