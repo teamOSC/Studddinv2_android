9a54988abec02bc573f0d00aca016adaf1176d7c
Arnav Gupta <championswimmer@gmail.com>
2015-01-26 23:14:18 +0530

    update event data facebook api
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- app/src/main/java/in/tosc/studddin/externalapi/FbApi.java
+++ app/src/main/java/in/tosc/studddin/externalapi/FbApi.java
@@ -3,10 +3,15 @@ package in.tosc.studddin.externalapi;
 import android.content.Context;
 import android.os.Bundle;
 
+import com.facebook.HttpMethod;
 import com.facebook.Request;
 import com.facebook.Response;
 import com.facebook.Session;
 import com.facebook.model.GraphUser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by championswimmer on 26/1/15.
@@ -45,8 +50,28 @@ public class FbApi {
         return;
     }
 
+    public static void getFacebookUserEvents (final FbGotEventDataCallback fgedc) {
+        JSONArray jsonArray;
+        Request r = new Request(session, "/me/events",null,
+                HttpMethod.GET, new Request.Callback() {
+            @Override
+            public void onCompleted(Response response) {
+                try {
+                    fgedc.gotEventData(response.getGraphObject().getInnerJSONObject().getJSONArray("data"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        r.executeAsync();
+    }
+
     public interface FbGotDataCallback {
         public void gotData (Bundle b);
+    }
+
+    public interface FbGotEventDataCallback {
+        public void gotEventData (JSONArray jArray);
     }
 
 
