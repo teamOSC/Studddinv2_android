bf055431c03204b4d4a975cc3dba45139f3af484
Arnav Gupta <championswimmer@gmail.com>
2015-01-26 21:40:19 +0530

    add api wrappers
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ app/src/main/java/in/tosc/studddin/externalapi/FbApi.java
@@ -0,0 +1,8 @@
+package in.tosc.studddin.externalapi;
+
+/**
+ * Created by championswimmer on 26/1/15.
+ */
+public class FbApi {
+    public static final String APP_ID = "903137443064438";
+}
---
+++ app/src/main/java/in/tosc/studddin/externalapi/TwttrApi.java
@@ -0,0 +1,7 @@
+package in.tosc.studddin.externalapi;
+
+/**
+ * Created by championswimmer on 26/1/15.
+ */
+public class TwttrApi {
+}
