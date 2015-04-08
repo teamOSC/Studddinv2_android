c3716e83f5ab7dc79b811c65d65865875b3d30b4
Umair Khan <omerjerk@gmail.com>
2015-04-08 23:04:26 +0530

    add layout in ItemSelectorFragment
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- app/src/main/java/in/tosc/studddin/fragments/signon/ItemSelectorFragment.java
+++ app/src/main/java/in/tosc/studddin/fragments/signon/ItemSelectorFragment.java
@@ -1,10 +1,17 @@
 package in.tosc.studddin.fragments.signon;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import in.tosc.studddin.R;
 
@@ -21,6 +28,12 @@ public class ItemSelectorFragment extends Fragment {
 
     public static final String TYPE = "type";
 
+    private RecyclerView interestsRecyclerView;
+    private RecyclerView.Adapter interestsAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private Activity parentActivity;
+
     public ItemSelectorFragment() {
         // Required empty public constructor
     }
@@ -35,7 +48,49 @@ public class ItemSelectorFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        parentActivity = getActivity();
         incomingBundle = getArguments();
+
+        interestsRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view_interests);
+        interestsRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(parentActivity);
+        interestsRecyclerView.setLayoutManager(mLayoutManager);
         return rootView;
+    }
+
+    public static class InterestsAdapter extends RecyclerView.Adapter<InterestsAdapter.ViewHolder> {
+        private ArrayList<String> interests;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public CheckBox mCheckBox;
+            public ViewHolder(CheckBox v) {
+                super(v);
+                mCheckBox = v;
+            }
+        }
+
+        public InterestsAdapter(ArrayList<String> myDataset) {
+            interests = myDataset;
+        }
+
+        @Override
+        public InterestsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_item_selector, parent, false);
+            CheckBox mCheckBox = (CheckBox) v.findViewById(R.id.checkbox_interest);
+            ViewHolder vh = new ViewHolder(mCheckBox);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.mCheckBox.setText(interests.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return interests.size();
+        }
     }
 }
